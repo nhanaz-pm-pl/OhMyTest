3b4c70a3e55ed64b5f8ccf7d9cdbab2e43046438
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-06-03 00:08:45 +0700

    Task cancel

--- src/NhanAZ/OhMyTest/TestTask.php
+++ src/NhanAZ/OhMyTest/TestTask.php
@@ -6,6 +6,7 @@ namespace NhanAZ\OhMyTest;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
+use pocketmine\Server;
 
 class TestTask extends Task {
 
@@ -13,5 +14,12 @@ class TestTask extends Task {
 	}
 
 	public function onRun(): void {
+		$server = Server::getInstance();
+		$tick = $server->getTick();
+		$server->broadcastMessage("[OhMyTest] I've run on tick " . $tick);
+		if ($tick == 60) {
+			$server->broadcastMessage("[OhMyTest] Task canceled");
+			$this->getHandler()->cancel();
+		}
 	}
 }
